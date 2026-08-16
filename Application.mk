9528475c888a62a75b4fae48de2f7af7cf59d292
dtd442 <106030458+dtd442@users.noreply.github.com>
2026-08-17 00:01:42 +0800

    Update Application.mk

--- Application.mk
+++ Application.mk
@@ -16,6 +16,6 @@
 APP_OPTIM := release
 APP_PLATFORM := android-29
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
-APP_CFLAGS := -O3
+APP_CFLAGS := -O3 -DPKGNAME=com/freedom/boost/services -DCLSNAME=TProxyService
 APP_SUPPORT_FLEXIBLE_PAGE_SIZES := true
 NDK_TOOLCHAIN_VERSION := clang
